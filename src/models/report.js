e4d84e622a0156e8feb977bc00bc867188c4e3b8
Ryan Urabe <rurabe@gmail.com>
2016-09-17 12:26:40 -1000

    default params

--- src/models/report.js
+++ src/models/report.js
@@ -5,11 +5,13 @@ const DB = require('../db');
 
 class Report {
   static create(params){
+    params = params || {};
     const q = Squel.insert().into('reports').setFields(params).returning('*');
     return DB.query(q.toParam());
   }
 
   static fetch(criteria){
+    criteria = criteria || {};
     let q = Squel.select().from('reports').order('created_at',false); // false makes it sort desc
     if(criteria.from){ q = q.where('created_at > ?',criteria.from) }
     return DB.query(q.toParam());
